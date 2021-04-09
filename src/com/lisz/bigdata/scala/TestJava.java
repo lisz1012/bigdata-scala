d51184a44311cfbaf96ed74be714c9ed46ad6d72
lisz1012 <lisz1012@163.com>
2021-04-08 21:14:43 -0700

    Initial commit

---
+++ src/com/lisz/bigdata/scala/TestJava.java
@@ -0,0 +1,7 @@
+package com.lisz.bigdata.scala;
+
+public class TestJava {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/com/lisz/bigdata/scala/TestScala.scala
@@ -0,0 +1,8 @@
+package com.lisz.bigdata.scala
+
+// In Scala, the main method must be put in an object, which is a static singleton
+object TestScala {
+  def main(args: Array[String]): Unit = {
+    println("Hello world!")
+  }
+}
