ebcbd385788b6bea7497ee14bedd94b72fe71f59
lisz1012 <lisz1012@163.com>
2021-04-12 22:37:25 -0700

    Frequency count

--- src/main/scala/com/lisz/bigdata/spark/WordCountScala.scala
+++ src/main/scala/com/lisz/bigdata/spark/WordCountScala.scala
@@ -13,19 +13,26 @@ object WordCountScala {
     //单词统计
     //DATASET
     val fileRDD: RDD[String] = context.textFile("data/testdata.txt")
-//    //hello world
-//    val words = fileRDD.flatMap((x: String) => {
-//      x.split("\\s+")
-//    })
-//    //hello
-//    //world
-//    val pairWords = words.map((x: String) => (x, 1))
-//    // (hello, 1)
-//    // (hello, 1)
-//    // (world, 1)
-//    val res = pairWords.reduceByKey((x: Int, y: Int) => x + y)
-//
-//    res.foreach(println)
-    fileRDD.flatMap(_.split("\\s+")).map((_,1)).reduceByKey(_+_).foreach(println)
+    //hello world
+    val words = fileRDD.flatMap((x: String) => {
+      x.split("\\s+")
+    })
+    //hello
+    //world
+    val pairWords = words.map((x: String) => (x, 1))
+    // (hello, 1)
+    // (hello, 1)
+    // (world, 1)
+    val res = pairWords.reduceByKey((x: Int, y: Int) => x + y)
+
+    res.foreach(println)
+    //fileRDD.flatMap(_.split("\\s+")).map((_,1)).reduceByKey(_+_).foreach(println)
+    //fileRDD.flatMap(_.split("\\s+")).map((_,1)).reduceByKey(_+_).map((x)=>(x._2,1)).reduceByKey(_+_).foreach(println)
+
+    // RDD can be reused
+    val resOver = res.map((x) => (x._2, 1)).reduceByKey(_ + _)
+    resOver.foreach(println)
+
+    Thread.sleep(Long.MaxValue)
   }
 }
