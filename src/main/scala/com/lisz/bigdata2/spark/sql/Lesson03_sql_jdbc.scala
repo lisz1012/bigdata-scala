1b350a9e53a9dea0a2727a6dc431a47cf117d0c0
lisz1012 <lisz1012@163.com>
2022-01-01 16:27:17 -0800

    Add notes

--- src/main/scala/com/lisz/bigdata2/spark/sql/Lesson03_sql_jdbc.scala
+++ src/main/scala/com/lisz/bigdata2/spark/sql/Lesson03_sql_jdbc.scala
@@ -20,7 +20,7 @@ object Lesson03_sql_jdbc {
     prop.put("password", "P@ssw0rd")
     prop.put("driver", "com.mysql.cj.jdbc.Driver")
     // 无论什么数据源，拿到的都是DS或者DF
-    val jdbcDF = session.read.jdbc(prop.get("url").toString, "student", prop)
+    val jdbcDF = session.read.jdbc(prop.get("url").toString, "student", prop) // student是MySQL中已存在的表
     jdbcDF.createTempView("aaa")
     session.sql("select name from aaa").show()
     // 把从JDBC读到的数据再写回MySQL，指定一个新的表名："aaa"
