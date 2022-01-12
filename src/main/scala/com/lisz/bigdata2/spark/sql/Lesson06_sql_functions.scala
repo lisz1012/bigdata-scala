7ab73568a360ae805ca1f97c643541d98c0e1322
lisz1012 <lisz1012@163.com>
2022-01-12 01:52:10 -0800

    Add notes

--- src/main/scala/com/lisz/bigdata2/spark/sql/Lesson06_sql_functions.scala
+++ src/main/scala/com/lisz/bigdata2/spark/sql/Lesson06_sql_functions.scala
@@ -111,7 +111,7 @@ object Lesson06_sql_functions {
     val res = session.sql("select ta.name, ta.class, tb.score + 20 + 80 from (select name, class from users) as ta " +
       "join (select name, score from users where score > 10) as tb " +
       "on ta.name = tb.name" +
-      " where tb.score > 60") // 单表关联、自关联，一张表里面既有发款记录也有对方的收款记录
+      " where tb.score > 60") // 单表关联、自关联，一张表里面既有发款记录也有对方的收款记录，银行用的很多
     res.show
     res.explain(true)
 
