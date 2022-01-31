ed6611d232978b375d7ca6df3dc11a7aef77f4df
lisz1012 <lisz1012@163.com>
2022-01-30 23:27:33 -0800

    Spark Streaming

--- src/main/scala/com/lisz/bigdata2/spark/streaming/Lesson03_DStream_API_3.scala
+++ src/main/scala/com/lisz/bigdata2/spark/streaming/Lesson03_DStream_API_3.scala
@@ -1,5 +1,6 @@
 package com.lisz.bigdata2.spark.streaming
 
+import org.apache.spark.broadcast.Broadcast
 import org.apache.spark.streaming.{Duration, StreamingContext}
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -33,22 +34,39 @@ object Lesson03_DStream_API_3 {
       RDD是一个单向链表，DStream也是一个单向链表，如果把最后一个DStream给ssc，那么ssc可以启动一个独立的线程去while(true){最后一个DStream遍历的过程} 而DStream对象里的函数就会随着循环而被多次执行
      */
 
-    val bc = sc.broadcast((1 to 5) toList)
+//    val bc = sc.broadcast((1 to 5) toList)
+    var bc : Broadcast[List[Int]] = null
+
     var jobNum = 0 // 怎么令jobNum随着job的提交执行而递增
     println("aaaa")  // application级别，只打印一次
 //    val res = format.filter(x => {
 //      bc.value.contains(x._2)
 //    })
-  val res = format.transform( // 每Job调用一次。 println(bbbb), println(cccc)被包裹在了DStream对象里，而res这个DStream对象在print中被注册到了StreamingContext线程里面去了，那边只要while循环起来就一次次的把bbbb和cccc打印了
-    rdd => {
-      // 函数是每job级
-      println("bbbb") // job级别每秒钟打印一次
-      rdd.map(x=>{
-        println("cccc") // rdd级别，每条记录x就触发一次（3次）
-        x
-      })
-    }
-  )
+    // 每Job调用一次。 println(bbbb), println(cccc)被包裹在了DStream对象里，而res这个DStream对象在print中被注册到了StreamingContext线程里面去了，那边只要while循环起来就一次次的把bbbb和cccc打印了
+//    val res = format.transform(
+//      rdd => {
+//        // 函数是每job级
+//        println("bbbb") // job级别每秒钟打印一次
+//        rdd.map(x=>{
+//          println("cccc") // rdd级别，每条记录x就触发一次（3次）
+//          x
+//        })
+//      }
+//    )
+
+
+    val res = format.transform(
+      rdd => {
+        jobNum += 1  // 每job级别增加1，在ssc的另一个while(true)循环的线程里，Driver端执行的
+        println(s"jobNum: $jobNum")
+        if (jobNum <= 5) {
+          bc = sc.broadcast((1 to 5).toList)
+        } else {
+          bc = sc.broadcast((6 to 15).toList)
+        }
+        rdd.filter(x=>bc.value.contains(x._2)) // 无论多少次job的执行，都是相同的bc，只有RDD接收的函数才是task级别，在Executor端的
+      }
+    )
 
 //    val res = format.transform(rdd => { // DStream包装的就是个RDD
 //      rdd.map(x=>(x._1, x._2*10))
