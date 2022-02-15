487c7a306a191372280b24faa9d5ed62952e9864
lisz1012 <lisz1012@163.com>
2022-02-14 18:20:23 -0800

    Spark Streaming offset 的正确提交方式

--- src/main/scala/com/lisz/bigdata2/spark/streaming/Lesson05_Spark_Kafka_Consumer.scala
+++ src/main/scala/com/lisz/bigdata2/spark/streaming/Lesson05_Spark_Kafka_Consumer.scala
@@ -1,11 +1,14 @@
 package com.lisz.bigdata2.spark.streaming
 
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import java.util
+
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord, OffsetAndMetadata, OffsetCommitCallback}
 import org.apache.kafka.clients.consumer.ConsumerConfig.{AUTO_OFFSET_RESET_CONFIG, ENABLE_AUTO_COMMIT_CONFIG, GROUP_ID_CONFIG, MAX_POLL_RECORDS_CONFIG}
+import org.apache.kafka.common.TopicPartition
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.SparkConf
 import org.apache.spark.streaming.dstream.InputDStream
-import org.apache.spark.streaming.kafka010.{CanCommitOffsets, ConsumerStrategies, HasOffsetRanges, KafkaUtils, LocationStrategies}
+import org.apache.spark.streaming.kafka010.{CanCommitOffsets, ConsumerStrategies, HasOffsetRanges, KafkaUtils, LocationStrategies, OffsetRange}
 import org.apache.spark.streaming.{Duration, StreamingContext}
 
 object Lesson05_Spark_Kafka_Consumer {
@@ -27,7 +30,7 @@ object Lesson05_Spark_Kafka_Consumer {
       (ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer]),
       (AUTO_OFFSET_RESET_CONFIG, "earliest"),
       (ENABLE_AUTO_COMMIT_CONFIG, "FALSE"),
-      (GROUP_ID_CONFIG, "g15"),
+      (GROUP_ID_CONFIG, "g16"),
       (MAX_POLL_RECORDS_CONFIG, "1")
     )
     val kafka: InputDStream[ConsumerRecord[String, String]]= KafkaUtils.createDirectStream(
@@ -47,18 +50,46 @@ object Lesson05_Spark_Kafka_Consumer {
     })
     dstream.print
 
+    var ranges: Array[OffsetRange] = null
     // 完成了业务代码后提交offset
-
+    // 正确的。放在dstream对象的接受函数里，那么未来在调度线程里，这个函数的每一个job都有机会调用一次，伴随着提交offset
+    // 那个时间点才会用维护的offset？维护offset的语义是持久化，它在application或者Driver重启的时候会被用到
     kafka.foreachRDD(
       rdd => {
+        println("foreachRDD...fun...")
         // Driver端可以拿到offset
-        val ranges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+        ranges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
         // 闭包, 通过KafkaUtils得到的第一个DStream向上转型，提交offset
-        kafka.asInstanceOf[CanCommitOffsets].commitAsync(ranges)
+        kafka.asInstanceOf[CanCommitOffsets].commitAsync(ranges, new OffsetCommitCallback {
+          override def onComplete(offsets: util.Map[TopicPartition, OffsetAndMetadata], exception: Exception): Unit = {
+            if (offsets != null) {
+              ranges.foreach(println)
+              println("-----------------")
+              val iter = offsets.keySet().iterator
+              while (iter.hasNext) {
+                val k = iter.next
+                val v = offsets.get(k)
+                println(s"${k.partition} ... ${v.offset}")
+//                if (v.offset % 5 == 0) throw new RuntimeException("Exception")
+              }
+            }
+          }
+        }) // 放到外面去，其实不会起到作用
+        // 如果维护到第三方MySQL：
+        /*
+          开启事务
+          提交数据
+          提交offset
+          commit
+         */
+        // 提交两头都要堵。每次Subscribe的时候，要去读数据库，读出offset，以TopicPartition为key，offset为value，传进Subscribe方法
       }
     )
 
+    // 上面的代码压在另一个线程里执行
     ssc.start()
+    Thread.sleep(10000)
+
     ssc.awaitTermination()
   }
 }
