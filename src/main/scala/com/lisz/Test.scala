fd84e5578fd65beaa1d5879066c05be622fbb93b
lisz1012 <lisz1012@163.com>
2022-10-12 23:40:48 -0700

    Add notes

---
+++ src/main/scala/com/lisz/Test.scala
@@ -0,0 +1,13 @@
+package com.lisz
+
+object Test {
+  def main(args: Array[String]): Unit = {
+    val listStr = List("Hello world", "Hello scala", "good idea")
+    val listStr2 = listStr.flatMap(_.split("\\s+")).map((_, 1)).groupBy((x) => x._1).map(x => {
+      val word = x._1
+      val count = x._2.map(t => t._2).sum
+      (word, count)
+    })
+    listStr2.foreach(println)
+  }
+}
