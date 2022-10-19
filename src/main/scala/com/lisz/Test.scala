534d857834205339f2ec494f24cf8651ca59ba5a
lisz1012 <lisz1012@163.com>
2022-10-18 23:49:39 -0700

    Add notes

--- src/main/scala/com/lisz/Test.scala
+++ src/main/scala/com/lisz/Test.scala
@@ -1,6 +1,6 @@
 package com.lisz
 
-object Test { // 这个演示不一定成功，由于依赖的问题, 跟spark版本有关，程序本身没啥毛病。
+object Test { // 这个演示不一定成功，由于依赖的问题, 跟spark版本有关，程序本身没啥毛病。😄
   def main(args: Array[String]): Unit = {
     val listStr = List("Hello world", "Hello scala", "good idea")
     val listStr2 = listStr.flatMap(_.split("\\s+")).map((_, 1)).groupBy((x) => x._1).map(x => {
